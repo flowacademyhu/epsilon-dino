00a2e7ba93a49ceb7023f9c33688a0f617bd189b
wunderszindi <sandrine.varga@gmail.com>
2019-02-19 16:51:11 +0100

    tomb fuggvenyek

---
+++ arrays.js
@@ -0,0 +1,27 @@
+const generate2D = (size, size2) => {
+  let arr = new Array(size);
+  for (let i = 0; i < size; i++) {
+    arr[i] = new Array(size2);
+  }
+ 
+  return arr;
+ };
+ const fill2D = (arr) => {
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      arr[x][y] = 0;
+    }
+  }
+  return arr;
+ };
+ let arr = fill2D(generate2D(10, 60));
+ const print2D = () => {
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      process.stdout.write(arr[x][y] + '');
+    }
+ 
+    console.log();
+  }
+ };
+ print2D();
