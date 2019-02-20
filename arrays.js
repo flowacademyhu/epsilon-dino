35b08226fae9dfbb06bd7fa3e61afa7de9db6227
Poczok <tamaspece@gmail.com>
2019-02-20 15:05:24 +0100

    time interval added

--- arrays.js
+++ arrays.js
@@ -4,25 +4,30 @@ const generate2D = (size, size2) => {
   for (let i = 0; i < size; i++) {
     arr[i] = new Array(size2);
   }
- 
+
   return arr;
- };
- const fill2D = (arr) => {
+};
+const fill2D = (arr) => {
   for (let x = 0; x < arr.length; x++) {
     for (let y = 0; y < arr[x].length; y++) {
       arr[x][y] = 0;
     }
   }
   return arr;
- };
- let arr = fill2D(generate2D(10, 60));
- const print2D = () => {
+};
+let arr = fill2D(generate2D(10, 60));
+const print2D = () => {
   for (let x = 0; x < arr.length; x++) {
     for (let y = 0; y < arr[x].length; y++) {
       process.stdout.write(arr[x][y] + '');
     }
- 
+
     console.log();
   }
- };
- print2D();
+};
+function intervalFunc () {
+  console.clear();
+  // minden
+  print2D();
+}
+setInterval(intervalFunc, 1500);
