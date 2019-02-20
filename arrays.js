e3afbed155c27be8b917b15513ed517643f18701
Poczok <tamaspece@gmail.com>
2019-02-20 16:49:42 +0100

    lala

--- arrays.js
+++ arrays.js
@@ -25,9 +25,44 @@ const print2D = () => {
     console.log();
   }
 };
+/*
+let sor = 1;
+let sor2 = 0;
+let mozgas = (arr) => {
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      arr[x][sor] = 1;
+      process.stdout.write(arr[x][y] + '');
+      arr[x][sor] = arr[x][sor2];
+      process.stdout.write(arr[x][y] + '');
+    }
+    console.log();
+  }
+  sor++;
+  sor2++;
+};
+*/
+let sor = 1;
+let sor2 = 0;
+const szalami = (arr) => {
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length-1; y++) {
+      arr[x][y] = arr[x][y+1];
+    }
+    sor++;
+    sor2++;
+  }
+};
+const szalami2 = (arr) => {
+  arr[5][5] = 1;
+};
+szalami2(arr);
 function intervalFunc () {
   console.clear();
   // minden
-  print2D();
+  // mozgas(arr);
+  
+  szalami(arr);
+  print2D(arr);
 }
 setInterval(intervalFunc, 1500);
